6cb3909a74e6b984619266673579e81dab922ab4
soma-y1029 <57809819+soma-y1029@users.noreply.github.com>
2020-02-13 21:59:32 -0800

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Soma Yoshida on 2/13/20.
+//  Copyright © 2020 Soma Yoshida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
